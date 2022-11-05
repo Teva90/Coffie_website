cac194f232e8eaf29b0024b73f44cecc6a60148b
Teva90 <mostafaayman551@gmail.com>
2022-11-05 21:37:27 +0200

    add all

---
+++ script.js
@@ -0,0 +1,17 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle("active");
+} 
+let cartItem = document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+}
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
